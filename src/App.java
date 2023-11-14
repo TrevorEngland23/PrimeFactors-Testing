8d678da1f88a8e240cb0a3d7718bc6cafd2845c0
TrevorEngland23 <trevor.england.523@gmail.com>
2023-11-13 23:28:09 -0800

    new feat: add ability for user to enter number to calculate prime factors

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Enter an integer between 0-1000: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int userInput = scanner.nextInt();
+
+        List<Integer> primeFactors = getPrimeFactors(userInput);
+
+        System.out.println(primeFactors);
+
+        scanner.close();
     }
+
+    public static List<Integer> getPrimeFactors(int number){
+        PrimeFactors determinePrimeFactors = new PrimeFactors();
+        return determinePrimeFactors.resolve(number);
+    }
+
 }
